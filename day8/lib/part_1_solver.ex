46c3f912818f66a6766f04d34a904062dd1f69e9
nikfp <nik@nikfp.com>
2024-11-30 21:39:38 -0700

    building on day 8 part 1 solver

---
+++ day8/lib/part_1_solver.ex
@@ -0,0 +1,11 @@
+defmodule Part1Solver do
+  def solve(
+        %{
+          instructions: inst,
+          locations: locs,
+          starting_loc: start
+        } = puzzle_input
+      ) do
+    puzzle_input
+  end
+end
--- day8/run.exs
+++ day8/run.exs
@@ -1,9 +1,12 @@
 Code.require_file("./lib/parser.ex", __DIR__)
+Code.require_file("./lib/part_1_solver.ex", __DIR__)
 
 alias Parser
+alias Part1Solver
 
 input = Parser.parse("./inputs/test1.txt")
 
 Map.update(input, :locations, %{}, fn locs -> Parser.locations_list_to_map(locs) end)
 |> Map.put(:starting_loc, "AAA")
+|> Part1Solver.solve()
 |> IO.inspect()
