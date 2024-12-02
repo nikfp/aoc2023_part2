2e381c44b126c72d579247b44c969bb04a7f05af
nikfp <nik@nikfp.com>
2024-12-01 18:02:36 -0700

    solved day 8 part 1

--- day8/lib/part_1_solver.ex
+++ day8/lib/part_1_solver.ex
@@ -2,10 +2,53 @@ defmodule Part1Solver do
   def solve(
         %{
           instructions: inst,
-          locations: locs,
           starting_loc: start
         } = puzzle_input
       ) do
-    puzzle_input
+    process(puzzle_input, inst, start, 0)
+  end
+
+  defp process(_data, _moves, "ZZZ", move_count) do
+    move_count
+  end
+
+  defp process(
+         %{locations: locs} = data,
+         [next_move | rest_moves],
+         current_loc,
+         move_count
+       ) do
+    case next_move do
+      "L" ->
+        {next_loc, _} = Map.get(locs, current_loc)
+
+        process(data, rest_moves, next_loc, move_count + 1)
+
+      "R" ->
+        {_, next_loc} = Map.get(locs, current_loc)
+
+        process(data, rest_moves, next_loc, move_count + 1)
+    end
+  end
+
+  defp process(
+         %{locations: locs, instructions: inst} = data,
+         [],
+         current_loc,
+         move_count
+       ) do
+    [next_move | rest_moves] = inst
+
+    case next_move do
+      "L" ->
+        {next_loc, _} = Map.get(locs, current_loc)
+
+        process(data, rest_moves, next_loc, move_count + 1)
+
+      "R" ->
+        {_, next_loc} = Map.get(locs, current_loc)
+
+        process(data, rest_moves, next_loc, move_count + 1)
+    end
   end
 end
--- day8/run.exs
+++ day8/run.exs
@@ -4,9 +4,21 @@ Code.require_file("./lib/part_1_solver.ex", __DIR__)
 alias Parser
 alias Part1Solver
 
-input = Parser.parse("./inputs/test1.txt")
+input1 = Parser.parse("./inputs/test1.txt")
+input2 = Parser.parse("./inputs/test2.txt")
 
+prod_input = Parser.parse("./inputs/prod.txt")
+
+solver = fn input -> 
 Map.update(input, :locations, %{}, fn locs -> Parser.locations_list_to_map(locs) end)
 |> Map.put(:starting_loc, "AAA")
 |> Part1Solver.solve()
-|> IO.inspect()
+end
+
+solver.(input1)
+|> IO.inspect(label: "Part 1 input 1")
+solver.(input2)
+|> IO.inspect(label: "Part 1 input 2")
+
+solver.(prod_input)
+|> IO.inspect(label: "Part 1 prod")
