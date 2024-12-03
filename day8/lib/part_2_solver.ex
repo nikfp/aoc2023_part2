aba81d89d4f34a54f4d1d715bf0e8a3e112215ac
nikfp <nik@nikfp.com>
2024-12-03 21:43:26 +0000

    solved day 8 part 2

--- day8/lib/part_2_solver.ex
+++ day8/lib/part_2_solver.ex
@@ -1,16 +1,74 @@
 defmodule Part2Solver do
   def solve(input) do
-    %{locations: locs} = input
+    %{locations: locs, instructions: inst} = input
 
     locs
     |> Map.keys()
     |> Enum.filter(fn key -> ends_with_char?(key, "A") end)
+    |> Enum.map(fn el ->
+      process(el, inst, 0, input)
+    end)
+    |> lcm_list()
+  end
+
+  defp gcd(a, b) when b == 0, do: abs(a)
+  defp gcd(a, b), do: gcd(b, rem(a, b))
+
+  defp lcm(0, 0), do: 0
+  defp lcm(a, b), do: div(abs(a * b), gcd(a, b))
+
+  defp lcm_list([]), do: 0
+  defp lcm_list([x]), do: x
+
+  defp lcm_list(numbers) do
+    Enum.reduce(numbers, &lcm/2)
   end
 
   defp ends_with_char?(str, char) do
     case str do
       <<_x::binary-size(2), letter::binary-size(1)>> when letter == char -> true
       _ -> false
+    end
+  end
+
+  defp process(pos, [], count, %{instructions: inst, locations: location_list} = data) do
+    [next_move | rest_moves] = inst
+
+    if ends_with_char?(pos, "Z") do
+      count
+    else
+      case next_move do
+        "L" ->
+          {new, _} = Map.get(location_list, pos)
+          new
+
+        "R" ->
+          {_, new} = Map.get(location_list, pos)
+          new
+      end
+      |> process(rest_moves, count + 1, data)
+    end
+  end
+
+  defp process(
+         pos,
+         [next_move | rest_moves],
+         count,
+         %{locations: location_list} = data
+       ) do
+    if ends_with_char?(pos, "Z") do
+      count
+    else
+      case next_move do
+        "L" ->
+          {new, _} = Map.get(location_list, pos)
+          new
+
+        "R" ->
+          {_, new} = Map.get(location_list, pos)
+          new
+      end
+      |> process(rest_moves, count + 1, data)
     end
   end
 end
--- day8/run.exs
+++ day8/run.exs
@@ -17,19 +17,26 @@ Map.update(input, :locations, %{}, fn locs -> Parser.locations_list_to_map(locs)
 |> Part1Solver.solve()
 end
 
-# solver.(input1)
-# |> IO.inspect(label: "Part 1 input 1")
-# solver.(input2)
-# |> IO.inspect(label: "Part 1 input 2")
-#
-# solver.(prod_input)
-# |> IO.inspect(label: "Part 1 prod")
+solver.(input1)
+|> IO.inspect(label: "Part 1 input 1")
+solver.(input2)
+|> IO.inspect(label: "Part 1 input 2")
+
+solver.(prod_input)
+|> IO.inspect(label: "Part 1 prod")
 
 part_2_test_input = Parser.parse("./inputs/test_part_2.txt")
 
-  part_2_test_input
+part_2_test_input
 |> Map.update(:locations, %{}, fn locs -> Parser.locations_list_to_map(locs) end)
 |> Part2Solver.solve()
 |>IO.inspect(label: "Part 2 input 1")
+
+IO.puts("\nStarting part 2 prod run")
+
+prod_input
+|> Map.update(:locations, %{}, fn locs -> Parser.locations_list_to_map(locs) end)
+|> Part2Solver.solve()
+|>IO.inspect(label: "Part 2 prod")
 
 
